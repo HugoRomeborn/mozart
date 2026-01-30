58fe3ba2c91cf18b82580bd746fa09ceef8c8384
Romebomben <hugo.romeborn@elev.ga.ntig.se>
2026-01-30 16:51:59 +0100

    Add basic routing and index page to HTTP server
    Introduces a simple routing mechanism to the HTTPServer class, serving static files from the 'views' directory and handling 404 and 502 errors. Adds an 'index.html' file as the root page.

--- tcp_server.rb
+++ tcp_server.rb
@@ -5,11 +5,18 @@ class HTTPServer
 
   def initialize(port)
     @port = port
+
+    @routes = {
+      "/" => "index.html",
+      "/what" => "",
+      "/about" => "<h1>How do I do this?</h1>\n<h2>Welcome to my life</h2>"
+    }
   end
 
   def start
     server = TCPServer.new(@port)
     puts "Listening on #{@port}"
+    Dir.chdir("views")
 
     while session = server.accept
       data = ''
@@ -24,16 +31,30 @@ class HTTPServer
 
       request = Request.new(data)
 
+      route = @routes[request.resource]
 
-      html = "<h1>Hello World!</h1>"
 
-      session.print "HTTP/1.1 200\r\n"
+      if route ==  nil
+        html = "<h1>ERROR 404 - page not found</h1>"
+        message = 404
+      elsif !File.exist?(route)
+        html = "<h1>ERROR 502 - Bad gateway"
+        message = 502
+      else
+        html = File.read(route)
+        message = 200
+      end
+      session.print "HTTP/1.1 #{message}\r\n"
       session.print "Content-Type: text/html\r\n"
       session.print "\r\n"
       session.print html
       session.close
     end
   end
+  
+  def parse_document(document)
+    
+  
 end
 
 server = HTTPServer.new(4567)
